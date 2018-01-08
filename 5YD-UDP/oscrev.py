1671f9288f50634c32a910bd9e63a9bcab491f6b
ray <ray@dac.tw>
2018-01-09 00:00:12 +0800

    Add broker role for /Hit

--- 5YD-UDP/oscrev.py
+++ 5YD-UDP/oscrev.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
-from OSC import OSCServer
+from OSC import *
 import sys
 from time import sleep
+import random
 
+cc = OSCClient()
+cc.connect(('192.168.12.249', 7000))   # localhost, port 57120
+
+def click(msg,arg0,arg1):
+    global cc
+    oscmsg = OSCMessage()
+    print "%s %d %d" % (msg,arg0,arg1)
+    oscmsg.setAddress(msg)
+    oscmsg.append(arg0)
+    oscmsg.append(arg1)
+    cc.send(oscmsg)
+    
 server = OSCServer( ("0.0.0.0", 7110) )
 server.timeout = 0
 run = True
@@ -20,11 +33,12 @@ server.handle_timeout = types.MethodType(handle_timeout, server)
 def hit_callback(path, tags, args, source):
     # which user will be determined by path:
     # we just throw away all slashes and join together what's left
-    user = ''.join(path.split("/"))
+    # user = ''.join(path.split("/"))
     # tags will contain 'fff'
     # args is a OSCMessage with data
     # source is where the message came from (in case you need to reply)
-    print (user, args[0], args[1], args[2]) 
+    print (path, args[0], args[1], args[2])
+    #click (path, args[1], args[2])    
 
 def quit_callback(path, tags, args, source):
     # don't do this at home (or it'll quit blender)
@@ -45,8 +59,10 @@ def each_frame():
 # simulate a "game engine"
 while run:
     # do the game stuff:
-    sleep(0.01)
+    #sleep(0.01)
     # call user script
     each_frame()
 
+click ("/Hit", random.randint(0,5120), random.randint(0,1920))
+    
 server.close()
